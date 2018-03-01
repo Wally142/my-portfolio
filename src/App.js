db3a6364de6faa5e717337b6582a78ab381b6e02
Greg.Wallerus <gwallerus@gmail.com>
2018-03-01 10:41:19 -0600

    projects added

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <h1> Welcome To My Portfolio! </h1>
-        <h2> Greg Wallerus </h2>
-        <p> I am a Full Stack Software Developer with a passion for learning. I primarily have worked with the MEAN stack but I am currently learning React and Java to further enhance my skills. </p>
-        <p> Feel free to check out my LinkedIn or GitHub, or send me a message if you would like to connect! </p>
-        <img alt="me" class="pic" src="./images/Greg.jpg" />
-        <br />
-        <a target="_blank" href="https://www.linkedin.com/in/gregwallerus/"><img alt="LinkedIn" class ="icon" src="./images/linkedIn.png" /></a>
-        <a target="_blank" href="https://github.com/Wally142"><img alt="GitHub" class="icon" src="./images/github.png" /></a>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome To My Portfolio</h1>
+          </header>
+          <h2> Greg Wallerus </h2>
+          <p> I am a Full Stack Software Developer with a passion for learning. I primarily have worked with the MEAN stack but I am currently learning React and Java to further enhance my skills. </p>
+          <p> Feel free to check out my LinkedIn or GitHub, or send me a message if you would like to connect! </p>
+          <img alt="me" class="pic" src="./images/Greg.jpg" />
+          <br />
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/gregwallerus/"><img alt="LinkedIn" class="icon" src="./images/linkedIn.png" /></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Wally142"><img alt="GitHub" class="icon" src="./images/github.png" /></a>
+          <h2> Projects </h2>
+        </div>
+        <div>
+          <h3> Happier Hours </h3>
+          <img alt="Happier Hours" class="pic1" src="./images/listView.png" />
+        </div>
+        <div>
+          <h3> Grow North</h3>
+          <img alt="Grow North" class="pic1" src="./images/dashboard.png" />
+        </div>
+        <div>
+          <h3> Wedding Website</h3>
+          <img alt="My Wedding Page" class="pic1" src="./images/wedding.png" />
+        </div>
       </div>
     );
   }
